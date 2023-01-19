f7e6ed9a2e91fe318a1dce3d753f62e22b58a417
yellaujg <yella.200180130@mhs.unimal.ac.id>
2023-01-19 14:36:02 +0700

    membuat halaman index account

--- resources/views/home/account/index.blade.php
+++ resources/views/home/account/index.blade.php
@@ -13,74 +13,74 @@
         </nav>
       </section>
 
-        <div class="card">
-          <div class="card-header">
-            <h5 class="card-title">Postingan Berita</h5>
-            <a href="{{ url('/blog/post/create') }}" role="button" class="btn btn-primary">
-              <i class="bi bi-plus-circle"></i> Postingan
-            </a>
-          </div>
+      <div class="card">
+        <div class="card-header">
+          <h5 class="card-title">Postingan Berita</h5>
+          <a href="{{ url('/blog/post/create') }}" role="button" class="btn btn-primary">
+            <i class="bi bi-plus-circle"></i> Postingan
+          </a>
+        </div>
 
-          <div class="card-body mt-2">
-            <div class="table-responsive">
-              <table class="table table-borderless datatable text-center">
-                <thead>
+        <div class="card-body mt-2">
+          <div class="table-responsive">
+            <table class="table table-borderless datatable text-center">
+              <thead>
+                <tr>
+                  <th class="text-center" scope="col">No</th>
+                  <th class="text-center" scope="col">Fullname</th>
+                  <th class="text-center" scope="col">Username</th>
+                  <th class="text-center" scope="col">E-Mail</th>
+                  <th class="text-center" scope="col">Phone</th>
+                  <th class="text-center" scope="col">Address</th>
+                  <th class="text-center" scope="col">Aksi</th>
+                </tr>
+              </thead>
+              
+              <tbody>
+                @foreach ($users as $user)
                   <tr>
-                    <th class="text-center" scope="col">No</th>
-                    <th class="text-center" scope="col">Fullname</th>
-                    <th class="text-center" scope="col">Username</th>
-                    <th class="text-center" scope="col">E-Mail</th>
-                    <th class="text-center" scope="col">Phone</th>
-                    <th class="text-center" scope="col">Address</th>
-                    <th class="text-center" scope="col">Aksi</th>
-                  </tr>
-                </thead>
-                
-                <tbody>
-                  @foreach ($users as $user)
-                    <tr>
-                      <td>{{ $loop->iteration }}</td>
-                      <td>{{ $user->fullname }}</td>
-                      <td>{{ $user->username }}</td>
-                      <td>{{ $user->email }}</td>
-                      <td>{{ $user->phone}}</td>
-                      <td>{{ $user->address}}</td>
-                      <td>
-                        <div class="btn-group btn-group-sm" role="group"
-                            aria-label="Basic example">
-                          <form action="{{-- url('/blog/post/show').'/'.$item->id --}}"
-                              method="GET">
-                              @csrf
-                              <button type="submit" class="btn btn-sm btn-success">
-                                  <i class="bi bi-eye"></i>
-                              </button>
-                          </form>
+                    <td>{{ $loop->iteration }}</td>
+                    <td>{{ $user->fullname }}</td>
+                    <td>{{ $user->username }}</td>
+                    <td>{{ $user->email }}</td>
+                    <td>{{ $user->phone}}</td>
+                    <td>{{ $user->address}}</td>
+                    <td>
+                      <div class="btn-group btn-group-sm" role="group"
+                          aria-label="Basic example">
+                        <form action="{{-- url('/blog/post/show').'/'.$item->id --}}"
+                            method="GET">
+                            @csrf
+                            <button type="submit" class="btn btn-sm btn-success">
+                                <i class="bi bi-eye"></i>
+                            </button>
+                        </form>
 
-                          <form action="{{-- url('/blog/post/edit').'/'.$item->id --}}"
-                              method="GET">
-                              @csrf
-                              <button type="submit" class="btn btn-sm btn-warning">
-                                  <i class="bi bi-pencil"></i>
-                              </button>
-                          </form>
-                          
-                          <form action="{{-- url('/blog/post/destroy').'/'.$item->id --}}"
-                              method="POST">
-                              @method('DELETE')
-                              @csrf
-                              <button type="submit" class="btn btn-sm btn-danger"
-                                  onclick="alert('Yakin untuk menghapus data ini?')">
-                                  <i class="bi bi-trash"></i>
-                              </button>
-                          </form>
-                        </div>
-                      </td>
-                    </tr>
-                  @endforeach
-                </tbody>
-              </table>
-            </div>
+                        <form action="{{-- url('/blog/post/edit').'/'.$item->id --}}"
+                            method="GET">
+                            @csrf
+                            <button type="submit" class="btn btn-sm btn-warning">
+                                <i class="bi bi-pencil"></i>
+                            </button>
+                        </form>
+                        
+                        <form action="{{-- url('/blog/post/destroy').'/'.$item->id --}}"
+                            method="POST">
+                            @method('DELETE')
+                            @csrf
+                            <button type="submit" class="btn btn-sm btn-danger"
+                                onclick="alert('Yakin untuk menghapus data ini?')">
+                                <i class="bi bi-trash"></i>
+                            </button>
+                        </form>
+                      </div>
+                    </td>
+                  </tr>
+                @endforeach
+              </tbody>
+            </table>
           </div>
+        </div>
       </div>
     </main>
 @endsection
