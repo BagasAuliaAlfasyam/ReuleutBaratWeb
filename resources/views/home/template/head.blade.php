e59c10fc8f8f7323dc061d4d50442a386253d6b7
Bryan <mhdalana211201@gmail.com>
2023-01-23 10:15:03 +0700

    menambahkan content meta

--- resources/views/home/template/head.blade.php
+++ resources/views/home/template/head.blade.php
@@ -3,8 +3,8 @@
     <meta content="width=device-width, initial-scale=1.0" name="viewport">
 
     <title>REULEUT BARAT | Dasbor</title>
-    <meta content="" name="description">
-    <meta content="" name="keywords">
+    <meta content="Website informasi Desa Reuleut Barat" name="description">
+    <meta content="Reuleut, Reuleut Barat, Desa, Aceh, Muara Batu, Desa binaan" name="keywords">
 
     <!-- Favicons -->
     <link href="{{ url('/assets/img/R.jpg') }}" rel="icon">
